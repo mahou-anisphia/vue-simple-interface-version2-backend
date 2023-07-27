7f86989d0438f9e23c4dc4ad048ba2ec3fce3d9f
AnisSamaDaisuki <youraisedmeup2004@gmail.com>
2023-07-27 12:42:26 +0700

    updated api

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+
 app.use(cors());
+app.use(bodyParser.json());
+
+var items = [
+  { text: "COS20007 Hurdle Test Resit" },
+  { text: "COS10025 DFD Diagram Presentation" },
+  { text: "COS10009 Introduction To Programming" },
+];
+
 app.get("/items", (req, res) => {
-  const items = [
-    { text: "COS20007 Hurdle Test Resit" },
-    { text: "COS10025 DFD Diagram Presentation" },
-    { text: "COS10009 Introduction To Programming" },
-  ];
   res.send(items);
+});
+
+app.post("/items", (req, res) => {
+  let temp = req.body;
+  console.log(temp);
+  items.push(temp);
+  res.json(temp);
 });
 app.listen(port, () => console.log("app running"));
