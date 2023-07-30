82770942b3565c7f9651a73eef7802e36bfad9c7
AnisSamaDaisuki <youraisedmeup2004@gmail.com>
2023-07-31 00:17:02 +0700

    added token and basic auth

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const jsonWebToken = require("jsonwebtoken");
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-var items = [];
+//using arrays to mimic database behaviour
+var items = [
+  {
+    unitCode: "MAH10009",
+    completionMethod: "Intro to Mahou",
+    email: "coding.students@example.com",
+    details: "Anisphia's magocology",
+    user: "admin",
+  },
+];
+var users = [{ username: "admin", password: "root" }];
 
 app.get("/items", (req, res) => {
   res.send(items);
@@ -17,9 +28,27 @@ app.get("/items/:id", (req, res) => {
 });
 
 app.post("/items", (req, res) => {
+  const userID = req.header("Authorization");
   let temp = req.body;
-  console.log(temp);
+  temp.user = users[userID].username;
   items.push(temp);
   res.json(temp);
 });
+
+app.post("/registers", (req, res) => {
+  let regData = req.body;
+  const existingUser = users.find((user) => user.username === regData.username);
+  if (existingUser) {
+    return res
+      .status(400)
+      .json({ error: "Username already exists. Registration failed." });
+  }
+
+  let newIndex = users.push(regData);
+  let regID = newIndex - 1;
+  let token = jsonWebToken.sign(regID, "vockey");
+
+  res.json(token);
+});
+
 app.listen(port, () => console.log("app running"));
